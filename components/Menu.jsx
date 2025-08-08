827c4d1889f06fc5586c0f2e41fc8acc9f73eec4
shreya <saumyakatkam16@gmail.com>
2025-08-08 15:10:55 +0530

    Feat: added all menu item images

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -7,6 +7,11 @@ const Menu = () => {
       <h2>Our Menu</h2>
       <img src="/images/menu/browine.jpg" alt="Browine" />
       <img src="/images/menu/cappuccino.jpg" alt="Cappuccino" />
+      <img src="/images/menu/donut.jpg" alt="Donut" />
+      <img src="/images/menu/icedamericano.jpg" alt="Iced Americano" />
+      <img src="/images/menu/flatwhite.jpg" alt="Flat White" />
+      <img src="/images/menu/mocha.jpg" alt="Mocha" />
+
       {/* Add more menu items here similarly */}
     </div>
   );
